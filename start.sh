011c55a865122d5bd372ec81750661128ef24e2a
autumn1023 <1563405868@qq.com>
2020-11-19 11:11:27 +0800

    fix

--- start.sh
+++ start.sh
@@ -1,12 +1,2 @@
 #! /usr/bin/perl
-exec(podname=`hostname`)
-exec(if [ ! -d /data/docker_upload ];then
-mkdir -p /data/docker_upload
-fi)
-
-exec(if [ -d /app/UnixBench/results ];then
-rm /app/UnixBench/results -rf 
-ln -sv /data/docker_upload/$podname /app/UnixBench/results
-fi)
-
 /app/UnixBench/Run -c 1 -c 4
