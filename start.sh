de2d5edc34188ed9e0b332e82242bec9f32751d1
autumn1023 <1563405868@qq.com>
2020-11-19 10:10:59 +0800

    fix

--- start.sh
+++ start.sh
@@ -1,1 +1,11 @@
+podname=`hostname`
+if [ ! -d /data/docker_upload ];then
+mkdir -p /data/docker_upload
+fi
+
+if [ -d /app/UnixBench/results ];then
+rm /app/UnixBench/results -rf 
+ln -sv /data/docker_upload/$podname /app/UnixBench/results
+fi
+
 /app/UnixBench/Run -c 1 -c 4
